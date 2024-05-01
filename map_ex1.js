86bae8ff28483f7a964c83e9a097ddac233f71ce
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2024-05-02 00:32:03 +0530

    Create map_ex1.js

---
+++ map_ex1.js
@@ -0,0 +1,15 @@
+// const input = [1, 2, 3, 4, 5];
+// const newArray = [];
+// for (let i = 0; i < input.length; i++) {
+//   newArray.push(input[i] * 2);
+// }
+// console.log(newArray);
+// normal way to double each element in an array
+//same program using map
+
+const input = [1, 2, 3, 4, 5];
+function twice(i) {
+  return i * 2;
+}
+const ans = input.map(twice);
+console.log(ans);
